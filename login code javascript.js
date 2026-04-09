177dfcfb88a4588956b4753136950c7ca6d98346
ABHIP822 <ap7258616@gmail.com>
2026-04-09 20:14:00 +0530

    Add files via upload

---
+++ login code javascript.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+const guestButton =
+document.getElementById('guestButton');
+guestButton.addEventListener('click',function() {
+    window.location.href = 'home.html';
+});
+})
---
+++ website intro javascript.js
@@ -0,0 +1,2 @@
+setTimeout(function()
+{window.location.href = "./login%20code%20html.html"},3000);
