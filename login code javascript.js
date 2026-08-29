3531a4111e526aa3f5555b3b61b2250089878765
ABHIP822 <ap7258616@gmail.com>
2026-08-29 17:24:48 +0530

    Delete login code javascript.js

--- login code javascript.js
+++
@@ -1,9 +0,0 @@
-
-
-document.addEventListener('DOMContentLoaded', function() {
-const guestButton =
-document.getElementById('guestButton');
-guestButton.addEventListener('click',function() {
-    window.location.href = 'home.html';
-});
-})
